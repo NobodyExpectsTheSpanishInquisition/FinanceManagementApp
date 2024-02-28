fd302c112bc43b53db52fd0e2e9fbe6875bd1da1
NobodyExpectsTheSpanishInquisition <87388101+NobodyExpectsTheSpanishInquisition@users.noreply.github.com>
2024-02-28 19:56:44 +0100

    feat(core): remove match clause

--- app/src/Core/RegisterFreeAccount/Presentation/RegisterAccountController.php
+++ app/src/Core/RegisterFreeAccount/Presentation/RegisterAccountController.php
@@ -4,10 +4,8 @@ declare(strict_types=1);
 
 namespace App\Core\RegisterFreeAccount\Presentation;
 
-use App\Core\RegisterFreeAccount\Application\CannotRegisterAccountException;
 use App\Core\RegisterFreeAccount\Application\RegisterFreeAccountCommand;
 use App\Core\RegisterFreeAccount\Application\RegisterFreeAccountHandler;
-use App\Core\Shared\Domain\ValueObject\AccountType;
 use App\Shared\Presentation\Http\AbstractController;
 use App\Shared\Presentation\HttpStatusCode;
 use Exception;
@@ -26,19 +24,16 @@ final class RegisterAccountController extends AbstractController
         try {
             $this->requestValidator->assertRequestIsValid($registerAccountRequest);
 
-            $accountType = $registerAccountRequest->accountType;
-            match ($accountType) {
-                AccountType::FREE => $registerFreeAccountHandler->handle(
-                    new RegisterFreeAccountCommand(
-                        $registerAccountRequest->accountId,
-                        $registerAccountRequest->userId,
-                        $registerAccountRequest->firstName,
-                        $registerAccountRequest->lastName,
-                        $registerAccountRequest->email,
-                        $registerAccountRequest->password,
-                    )
-                ),
-            };
+            $registerFreeAccountHandler->handle(
+                new RegisterFreeAccountCommand(
+                    $registerAccountRequest->accountId,
+                    $registerAccountRequest->userId,
+                    $registerAccountRequest->firstName,
+                    $registerAccountRequest->lastName,
+                    $registerAccountRequest->email,
+                    $registerAccountRequest->password,
+                )
+            );
         } catch (Exception $e) {
             return $this->exceptionConverter->convertToJsonResponse($e);
         }
